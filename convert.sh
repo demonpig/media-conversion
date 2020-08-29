e45f08778a0b21076baafe3531aa91e9da681874
Max Mitschke <maxmitschke@fastmail.com>
2020-08-29 02:48:41 -0400

    Updating structure and handbrake presets
    This new structure should be more helpful when converting files. I
    have also decided to go back to MP4 container instead of using the
    MKV. At the time of writing this commit, I am unaware of the
    reasoning of why I changed containers. From my testing, MP4 is
    better suited for streaming.

---
+++ convert.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eu
+shopt -s globstar
+
+# Variables
+BASEPATH="$(dirname $0)"
+MEDIA_FILES_DIR="files"
+MEDIA_HB_DIR="handbrake"
+MEDIA_PRESET="DemonPresets"
+MEDIA_PRESET_FILENAME="${1-default}" ; MEDIA_PRESET_FILENAME="${MEDIA_PRESET_FILENAME%.json}.json"
+NO_PARALLEL=0
+
+# Checks
+CMD_PL="/usr/bin/parallel"
+CMD_HB="/usr/bin/HandBrakeCLI"
+test -x $CMD_PL || export NO_PARALLEL=1
+test -x $CMD_HB || exit 1
+test -f "$BASEPATH/$MEDIA_HB_DIR/$MEDIA_PRESET_FILENAME" || exit 2
+
+# Functions
+function parallel_convert(){
+    $CMD_PL --eta --verbose -j2 HandBrakeCLI \
+        --preset-import-file "$BASEPATH/$MEDIA_HB_DIR/$MEDIA_PRESET_FILENAME" \
+        --preset "${MEDIA_PRESET}" \
+        -i {} -o $BASEPATH/$MEDIA_FILES_DIR/{/.}.mp4 ::: $BASEPATH/$MEDIA_FILES_DIR/*
+}
+
+function forloop_convert(){
+    for file in $BASEPATH/$MEDIA_FILES_DIR/*; do
+        HandBrakeCLI \
+            --preset-import-file "$BASEPATH/$MEDIA_HB_DIR/$MEDIA_PRESET_FILENAME" \
+            --preset "$MEDIA_PRESET" \
+            -i "$file" -o "${file%.*}.mp4"
+    done
+}
+
+# Main
+if [[ $NO_PARALLEL -eq 1 ]]; then
+    forloop_convert
+else
+    parallel_convert
+fi
